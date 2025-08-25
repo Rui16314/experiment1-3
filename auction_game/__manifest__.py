5f49d3c1abd143450c5545ddd5852b7e36f34a64
Rui16314 <rui315@yorku.ca>
2025-08-24 23:36:44 -0400

    Update __manifest__.py

--- auction_game/__manifest__.py
+++ auction_game/__manifest__.py
@@ -1,26 +1,31 @@
 from otree.api import *
 
 doc = """
-Auction experiments for ECON 3310.
+Auction experiment with 6 treatments and bot opponents.
 """
 
 class C(BaseConstants):
     NAME_IN_URL = 'auction_game'
-    PLAYERS_PER_GROUP = 2
-    NUM_ROUNDS = 10
+    PLAYERS_PER_GROUP = 1
+    NUM_ROUNDS = 60  # 6 experiments × 10 rounds
+    EXPERIMENTS = [1, 2, 3, 4, 5, 6]
+    ROUNDS_PER_EXPERIMENT = 10
+    VALUATION_RANGE = [i / 100 for i in range(101)]  # 0 to 100
 
 class Subsession(BaseSubsession):
-    pass
+    def creating_session(subsession):
+        for player in subsession.get_players():
+            player.experiment = ((player.round_number - 1) // C.ROUNDS_PER_EXPERIMENT) + 1
 
 class Group(BaseGroup):
-    valuation_p1 = models.FloatField()
-    valuation_p2 = models.FloatField()
-    bid_p1 = models.FloatField()
-    bid_p2 = models.FloatField()
-    winner = models.IntegerField()
+    valuation = models.FloatField()
+    bot_valuation = models.FloatField()
+    player_bid = models.FloatField()
+    bot_bid = models.FloatField()
+    winner = models.StringField()
     price_paid = models.FloatField()
-    payoff_p1 = models.FloatField()
-    payoff_p2 = models.FloatField()
+    payoff = models.FloatField()
 
 class Player(BasePlayer):
-    pass
+    experiment = models.IntegerField()
+
