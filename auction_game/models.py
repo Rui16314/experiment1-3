85f544d442cebc7806ff7944a55f131b177196f0
Rui16314 <rui315@yorku.ca>
2025-08-24 23:37:08 -0400

    Update models.py

--- auction_game/models.py
+++ auction_game/models.py
@@ -1,44 +1,51 @@
 from otree.api import *
+import random
 
 class C(BaseConstants):
     NAME_IN_URL = 'auction_game'
-    PLAYERS_PER_GROUP = 2
-    NUM_ROUNDS = 10
-    VALUATION_RANGE = [i / 100 for i in range(10001)]
+    PLAYERS_PER_GROUP = 1
+    NUM_ROUNDS = 60
+    EXPERIMENTS = [1, 2, 3, 4, 5, 6]
+    ROUNDS_PER_EXPERIMENT = 10
+    VALUATION_RANGE = [i / 100 for i in range(101)]
 
 class Subsession(BaseSubsession):
-    pass
+    def creating_session(subsession):
+        for player in subsession.get_players():
+            player.experiment = ((player.round_number - 1) // C.ROUNDS_PER_EXPERIMENT) + 1
 
 class Group(BaseGroup):
-    valuation_p1 = models.FloatField()
-    valuation_p2 = models.FloatField()
-    bid_p1 = models.FloatField()
-    bid_p2 = models.FloatField()
-    winner = models.IntegerField()
+    valuation = models.FloatField()
+    bot_valuation = models.FloatField()
+    player_bid = models.FloatField()
+    bot_bid = models.FloatField()
+    winner = models.StringField()
     price_paid = models.FloatField()
-    payoff_p1 = models.FloatField()
-    payoff_p2 = models.FloatField()
+    payoff = models.FloatField()
 
     def set_valuations(self):
-        import random
-        self.valuation_p1 = random.choice(C.VALUATION_RANGE)
-        self.valuation_p2 = random.choice(C.VALUATION_RANGE)
+        self.valuation = random.choice(C.VALUATION_RANGE)
+        self.bot_valuation = random.choice(C.VALUATION_RANGE)
 
-    def resolve_auction(self):
-        if self.bid_p1 > self.bid_p2:
-            self.winner = 1
-            self.price_paid = self.bid_p1  # first-price
-            self.payoff_p1 = self.valuation_p1 - self.price_paid
-            self.payoff_p2 = 0
-        elif self.bid_p2 > self.bid_p1:
-            self.winner = 2
-            self.price_paid = self.bid_p2
-            self.payoff_p2 = self.valuation_p2 - self.price_paid
-            self.payoff_p1 = 0
+    def set_bot_bid(self, experiment):
+        if experiment in [1, 2, 3]:  # First-price
+            self.bot_bid = round(self.bot_valuation / 2, 2)
+        else:  # Second-price
+            self.bot_bid = self.bot_valuation
+
+    def resolve_auction(self, experiment):
+        if self.player_bid > self.bot_bid:
+            self.winner = 'You'
+            self.price_paid = self.player_bid if experiment in [1, 2, 3] else self.bot_bid
+            self.payoff = round(self.valuation - self.price_paid, 2)
+        elif self.bot_bid > self.player_bid:
+            self.winner = 'Bot'
+            self.price_paid = self.bot_bid if experiment in [1, 2, 3] else self.player_bid
+            self.payoff = 0
         else:
-            self.winner = 0
+            self.winner = 'Tie'
             self.price_paid = 0
-            self.payoff_p1 = self.payoff_p2 = 0
+            self.payoff = 0
 
 class Player(BasePlayer):
-    pass
+    experiment = models.IntegerField()
