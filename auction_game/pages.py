c3521d7e649627a43fdea15eaacbf006834cd82f
Rui16314 <rui315@yorku.ca>
2025-08-24 23:37:30 -0400

    Update pages.py

--- auction_game/pages.py
+++ auction_game/pages.py
@@ -7,23 +7,28 @@ class Instructions(Page):
 
 class Bidding(Page):
     form_model = 'group'
-    form_fields = ['bid_p1', 'bid_p2']
+    form_fields = ['player_bid']
 
     def before_next_page(player, timeout_happened):
         group = player.group
         group.set_valuations()
-        group.resolve_auction()
+        group.set_bot_bid(player.experiment)
+        group.resolve_auction(player.experiment)
 
 class Results(Page):
     def vars_for_template(player):
         group = player.group
         return dict(
-            valuation=group.valuation_p1 if player.id_in_group == 1 else group.valuation_p2,
-            bid=group.bid_p1 if player.id_in_group == 1 else group.bid_p2,
-            opponent_bid=group.bid_p2 if player.id_in_group == 1 else group.bid_p1,
-            payoff=group.payoff_p1 if player.id_in_group == 1 else group.payoff_p2,
+            valuation=group.valuation,
+            bot_valuation=group.bot_valuation,
+            player_bid=group.player_bid,
+            bot_bid=group.bot_bid,
             winner=group.winner,
             price_paid=group.price_paid,
+            payoff=group.payoff,
+            experiment=player.experiment,
+            round_in_exp=((player.round_number - 1) % C.ROUNDS_PER_EXPERIMENT) + 1
         )
 
 page_sequence = [Instructions, Bidding, Results]
+
